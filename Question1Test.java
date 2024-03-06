97259cc2ff3bea5102c20b70eb423d791e10125e
Istvan Bathazi <istvan.bathazi@hotmail.com>
2024-03-05 21:50:00 -0500

    readme updates

--- Question1Test.java
+++ Question1Test.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 public class Question1Test {
 
     @Test
-    public void test1() {
+    public void testMt10() {
         // Test case 1
         int result1 = Question1.findOccurrences(10, 5);
         assertEquals(2, result1);
     }
 
     @Test
-    public void test2() {
+    public void testMt20() {
         // Test case 2
         int result2 = Question1.findOccurrences(20, 10);
         assertEquals(4, result2);
@@ -20,20 +20,32 @@ public class Question1Test {
 
     // Test case 3
     @Test
-    public void test3() {
+    public void testMt15() {
         int result3 = Question1.findOccurrences(15, 3);
         assertEquals(2, result3);
     }
 
     @Test
-    public void test4() {// Test case 4
+    public void testMt8() {// Test case 4
         int result4 = Question1.findOccurrences(8, 4);
         assertEquals(3, result4);
     }
 
     @Test
-    public void test5() { // Test case 5
+    public void testMt24() { // Test case 5
         int result5 = Question1.findOccurrences(24, 100);
         assertEquals(3, result5);
+    }
+
+    @Test
+    public void testMt25() { // Test case 5
+        int result5 = Question1.findOccurrences(25, 100);
+        assertEquals(5, result5);
+    }
+
+    @Test
+    public void testMt25_2() { // Test case 5
+        int result5 = Question1.findOccurrences(25, 120);
+        assertEquals(8, result5);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-test restults:
+### Multiplication Table
+
+#### Problem Statement:
+
+Suppose you have a multiplication table that is N by N. That is, a 2D array where the value at the i-th row and j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+
+Given integers N and X, write a function that returns the number of times X appears as a value in an N by N multiplication table.
+
+Examples
+Testcase 1:
+             Input: N = 6, X = 12
+             Output: 4, since there are four 12’s in a 6x6 multiplication table
+```
+| 1 |  2 |  3 |  4 |  5 |  6 |
+| 2 |  4 |  6 |  8 | 10 | 12 |
+| 3 |  6 |  9 | 12 | 15 | 18 |
+| 4 |  8 | 12 | 16 | 20 | 24 |
+| 5 | 10 | 15 | 20 | 25 | 30 |
+| 6 | 12 | 18 | 24 | 30 | 36 |
+```
+
+
+### Max Stock Loss
+
+#### Problem Statement:
+
+Given an array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum loss you could have from buying and selling that stock once. You must buy before you can sell it.
+
+#### Examples
+
+Testcase 1:
+             Input: [9, 11, 8, 5, 7, 10]
+Output: 6 since you could buy the stock at 11 dollars and sell it at 5 dollars.
+
+test results:
 ![image](https://github.com/ibathazi/highmark-interview/assets/41767606/0df141f4-bfd7-4674-b93a-58dc487c1428)
